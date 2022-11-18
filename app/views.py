f630fc28839de8c9d8be68a0fca2bd24ee687c44
Nathan Coxson <nathancoxson2739@gmail.com>
2022-11-17 21:06:02 -0500

    Added basic form to accept user route information and created a templates folder for html files.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def home():
-    return "Home page"
+    route = {}
+    if len(request.form) > 0:
+        route['start'] = request.form['currentLocation']
+        route['destination'] = request.form['destination']
+    return render_template('index.html', template_route=route)
