4a928a8a795a5c7d66f1c4ffb3d561febda3e897
Nathan Coxson <nathancoxson2739@gmail.com>
2022-12-02 17:05:16 -0500

    Added comments to user_autofill.js

--- app/static/user_autofill.js
+++ app/static/user_autofill.js
@@ -1,14 +1,32 @@
+/*
+    The function of this script is to attempt to get the user's
+    current location from the browser if they allow it. If permission
+    is granted, their location is taken as lat. and long. coords. and
+    then converted into an address using Google API's geocoding.
+    Once the address is obtained, the data is posted to the flask app
+    for further processing, and the address is autofilled into the 
+    text input field for starting location.
+*/
+
+//Should be the base url of the flask app
+const baseURL = 'hhtp://127.0.0.1:5000/';
+
 const onSuccess = async (result) => {
-    geocoder = new google.maps.Geocoder();
+    //Data object to be sent to flask app
     data = {
         latitude: result.coords.latitude,
         longitude: result.coords.longitude,
         address: ''
     }
+
+    //Geocode coords into address
+    geocoder = new google.maps.Geocoder();
     await geocoder.geocode({location: {lat: result.coords.latitude, lng: result.coords.longitude}}).then(response => {
         data.address = response.results[0].formatted_address;
     });
-    await fetch("http://127.0.0.1:5000/location", {
+
+    //Post data to flask app
+    await fetch(`${baseURL}/location`, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json',
@@ -16,13 +34,14 @@ const onSuccess = async (result) => {
         },
         body: JSON.stringify(data)
     });
-    //window.location = 'http://127.0.0.1:5000/';
+
+    //Set the value of starting location input
     document.getElementById('start_input').setAttribute('value', data.address);
 }
 
-//Don't allow for location processing until the whole DOM has loaded to ensure that Google Maps API has been defined
+//Don't allow for location processing until the whole DOM has loaded to ensure that Google Maps API has been defined.
 addEventListener('load', () => {
-    //Check if user has allowed access to location data
+    //Check if user has allowed access to location data and process their location if they have.
     if(window.navigator.geolocation) {
         window.navigator.geolocation.getCurrentPosition(onSuccess, () => console.log('Failed to get user location.'));
     }
