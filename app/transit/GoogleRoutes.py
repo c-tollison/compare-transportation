9e78aab477735c8259c799d28e778e8e25ef5e7f
Nathan Coxson <nathancoxson2739@gmail.com>
2022-12-05 19:29:33 -0500

    Refactored the GoogleRoutes file to include a function which accepts origin and destination parameters and returns formatted time and distance results for different transportation methods.

--- app/transit/GoogleRoutes.py
+++ app/transit/GoogleRoutes.py
@@ -23,8 +23,33 @@ import requests
 # print(response.text) ##prints JSON output to command line
 
 def getRoute(origin, destin):
+    key = 'API_KEY'
     payload={}
     headers = {}
-    url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+ origin +"&destinations="+ destin +"&mode=driving&units=imperial&key=" + "data"
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response.text
+    baseUrl = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destin}&units=imperial&key={key}"
+    driving = requests.request("GET", baseUrl+'&mode=driving', headers=headers, data=payload)
+    walking = requests.request("GET", baseUrl+'&mode=walking', headers=headers, data=payload)
+    bicycling = requests.request("GET", baseUrl+'&mode=bicycling', headers=headers, data=payload)
+    transit = requests.request("GET", baseUrl+'&mode=transit', headers=headers, data=payload)
+    results = {
+        'driving': {
+            'duration': driving.json()['rows'][0]['elements'][0]['duration']['text'],
+            'distance': driving.json()['rows'][0]['elements'][0]['distance']['text']
+        },
+        'walking': {
+            'duration': walking.json()['rows'][0]['elements'][0]['duration']['text'],
+            'distance': walking.json()['rows'][0]['elements'][0]['distance']['text']
+        },
+        'bicycling': {
+            'duration': bicycling.json()['rows'][0]['elements'][0]['duration']['text'],
+            'distance': bicycling.json()['rows'][0]['elements'][0]['distance']['text']
+        }
+    }
+    if not transit.json()['rows'][0]['elements'][0]['status'] == 'ZERO_RESULTS':
+        results['transit'] = {
+            'duration': transit.json()['rows'][0]['elements'][0]['duration']['text'],
+            'distance': transit.json()['rows'][0]['elements'][0]['distance']['text']
+        }
+    return results
+
+    #"https://maps.googleapis.com/maps/api/distancematrix/json?origins="+ origin +"&destinations="+ destin +"&mode=driving&units=imperial&key=" + key
